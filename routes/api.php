37a0a3a154a10151ed55bcebcd2dbd18daafb1e9
QTDev2139 <tranvietquan0102003@gmail.com>
2026-05-22 00:23:15 +0700

    setup RESTApis

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Api\TestController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/tests', [TestController::class, 'index']);
